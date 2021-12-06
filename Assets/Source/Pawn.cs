90e9df087511ee82c94bfdbdcc21423bf0bef7a5
c0sx <vallkhale@gmail.com>
2021-12-06 23:19:53 +0300

    feat: toggle cells

--- Assets/Source/Pawn.cs
+++ Assets/Source/Pawn.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 public class Pawn : MonoBehaviour
 {
     private MeshRenderer _mesh;
+    [SerializeField] private Color _current;
+    [SerializeField] private Color _previous;
 
-    public void Init(Color color)
+    private void OnMouseDown()
     {
+        var inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit)) {
+            ToggleSelection();
+        }
+    }
+
+    public void Init(Color color, Color previous)
+    {
+        _current = color;
+        _previous = previous;
+
         _mesh = GetComponent<MeshRenderer>();
-        _mesh.material.color = color;;
+        _mesh.material.color = color;
+    }
+
+    private void ToggleSelection()
+    {
+        var temp = _previous;
+        _previous = _current;
+        _current = temp;
+
+        _mesh.material.color = _current;
     }
 }
--- Assets/Source/PawnSpawner.cs
+++ Assets/Source/PawnSpawner.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class PawnSpawner : MonoBehaviour
 {
     [SerializeField] private Pawn _pawnPrefab;
-    [SerializeField] private Color _color;
+    [SerializeField] private Color _current;
+    [SerializeField] private Color _alternative;
 
     public Pawn Spawn()
     {
         var pawn = Instantiate<Pawn>(_pawnPrefab);
         
-        pawn.Init(_color);
+        pawn.Init(_current, _alternative);
 
         return pawn;
     }
